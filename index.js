5d77004123d8dfd7717d816fe781829daf2b5d38
mantra-gor <mantragor77@gmail.com>
2026-05-17 18:09:11 +0530

    clean version of code

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.use(express.json());
 app.use(cors());
 app.use("/", userRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 sequelize
   .sync()
   .then(() => {
     console.log("Database connected");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log("Server running on port", PORT);
     });
   })
